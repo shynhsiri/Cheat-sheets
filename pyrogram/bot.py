6c5bf4fbd3000e2dd6e60b147d6d9eb1d3429ec3
shynhsiri <shynhsiri@gmail.com>
2024-06-15 12:10:13 +0330

    6.0

--- pyrogram/bot.py
+++ pyrogram/bot.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 
 load_dotenv()
 bot =Client(
@@ -16,6 +17,8 @@ bot =Client(
 def start(bot, message): #function
     bot.send_message(message.chat.id, "Hello World")#method
 
+#---------------------------------------------------------------------------
+
 #  2/1-reply_text()
 
 @bot.on_message(filters.command('reply'))
@@ -28,6 +31,8 @@ def reply(bot, message):
 # def echo(client, message):
 #     message.reply_text(message.text)
 
+#---------------------------------------------------------------------------
+
 #  3-welcome
 
 GROUP = "#" #Group id/username and also can be a list
@@ -36,6 +41,8 @@ WELCOME_MESSAGE = "Welcome!" #message
 @bot.on_message(filters.chat(GROUP) & filters.new_chat_members) #filter new members
 def welcome(client, message):
     message.reply_text(WELCOME_MESSAGE)#pay attention in groups quote default is True
+
+#---------------------------------------------------------------------------
 
 # 4-media
 # be  sure to disable echo
@@ -61,14 +68,86 @@ def audiosend(bot, message):
     bot.send_audio(message.chat.id, "#")  #insert the id that given by bot
 # replace audio with data types in parentheses
 
+#---------------------------------------------------------------------------
+
 # 5-delete_message
 
+# @bot.on_message(filters.text)
+# def delete_text(bot, message):
+#     word_list =  ["fuck", "wtf"]
+#     if any(word in message.text.lower() for word in word_list):
+#         bot.delete_messages(message.chat.id, [message.id])
+#         bot.send_message(message.chat.id, "Blocklist word detected!")
+
+#---------------------------------------------------------------------------
+
+# 6/1-Inline_Keyboard
+# make sure delete_massage section is commented
+
+# inline keyboard is a keyboard that appears right below the message
+# it can be used to provide users with a set of options to interact with your bot
+# inline keyboard can be used to send callback data to the bot
+# callback data can be used to identify the button that was pressed
+
+
+# Define the /inline command handler
+@bot.on_message(filters.command("inline"))
+async def inline(client, message):
+    # Create an inline keyboard with 4 buttons
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Button 1", callback_data="btn1")],
+        [InlineKeyboardButton("Button 2", callback_data="btn2")],
+        [InlineKeyboardButton("Button 3", callback_data="btn3")],
+        [InlineKeyboardButton("Button 4", url="https://shynhsiri.github.io/")]
+    ])
+    # Send a message with the inline keyboard
+    await message.reply("Choose an option:", reply_markup=keyboard)
+
+# Define the callback query handler
+@bot.on_callback_query()
+async def callback_query(client, callback_query):
+    data = callback_query.data
+    if data == "btn1":
+        await callback_query.answer("You clicked Button 1")
+    elif data == "btn2":
+        await callback_query.answer("You clicked Button 2")
+    elif data == "btn3":
+        await callback_query.answer("You clicked Button 3")
+    elif data == "btn4":
+        await callback_query.answer("You clicked Button 4")
+    else:
+        await callback_query.answer("Unknown button")
+
+# 6/2-Reply_Keyboard
+# reply keyboard is a keyboard that appears at the bottom of the chat
+
+# Define the /repc command handler
+@bot.on_message(filters.command("repc"))
+async def start(client, message):
+    # Create a reply keyboard with 4 buttons
+    keyboard = ReplyKeyboardMarkup([
+        [KeyboardButton("Button 1")],
+        [KeyboardButton("Button 2")],
+        [KeyboardButton("Button 3")],
+        [KeyboardButton("Button 4")]
+    ], resize_keyboard=True)  # Resize the keyboard to fit the screen
+    # Send a message with the reply keyboard
+    await message.reply("Choose an option:", reply_markup=keyboard)
+
+# Define a message handler for the buttons
 @bot.on_message(filters.text)
-def delete_text(bot, message):
-    word_list =  ["fuck", "wtf"]
-    if any(word in message.text.lower() for word in word_list):
-        bot.delete_messages(message.chat.id, [message.id])
-        bot.send_message(message.chat.id, "Blocklist word detected!")
+async def handle_button(client, message):
+    text = message.text
+    if text == "Button 1":
+        await message.reply("You pressed Button 1")
+    elif text == "Button 2":
+        await message.reply("You pressed Button 2")
+    elif text == "Button 3":
+        await message.reply("You pressed Button 3")
+    elif text == "Button 4":
+        await message.reply("You pressed Button 4")
+    else:
+        await message.reply("Unknown button")
 
 # print in terminal to check code  is running 
 print("running")
